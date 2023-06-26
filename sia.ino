7b4265380ce641fdc1f2723da7325d0dc26e4595
Oxymoron <daniel.m.bihlmaier@gmail.com>
2023-06-26 06:07:26 +0200

    🚧 push-sort

--- sia.ino
+++ sia.ino
@@ -14,17 +14,21 @@ const int clawLimit = 2000;
 
 tcs3200 colorSensor(8, 9, 10, 11, 12); // (s0, s1, s2, s3, out)
 
-const int colorCount = 10;
-int colors[colorCount][3] = { //! colors[0] is ground
+const int blockCount = 9;
+const int totalColorCount = blockCount + 1; // plus ground
+int colors[totalColorCount][3] = {          //! colors[0] is ground
     {0, 0, 0},    {250, 250, 250}, {0, 0, 0},    {142, 34, 41}, {166, 125, 71},
     {35, 55, 38}, {150, 50, 43},   {22, 25, 45}, {0, 0, 0},     {0, 0, 0}};
 
-int colorNumbers[colorCount] = {-1, 0, 1, 2, 3, 4, 5, 6, 7, 8};
-String colorNames[colorCount] = {"ground",  "1-purple", "2-blue", "3-cyan",
-                                 "4-green", "5-yellow", "6-red",  "7-black",
-                                 "8-gray",  "9-white"};
+int colorNumbers[totalColorCount] = {-1, 0, 1, 2, 3, 4, 5, 6, 7, 8};
+String colorNames[totalColorCount] = {
+    "ground",   "1-purple", "2-blue",  "3-cyan", "4-green",
+    "5-yellow", "6-red",    "7-black", "8-gray", "9-white"};
 
-int colorPositions[colorCount - 1] = {-1, -1, -1, -1, -1, -1, -1, -1};
+int blockPositions[blockCount] = {-1, -1, -1, -1, -1, -1, -1, -1};
+
+const int blockScanOffset = 400; // steps
+const int blockWidth = 1000;     // steps
 
 void setup() {
   horizontal.setMaxSpeed(1000);
@@ -42,9 +46,16 @@ void loop() {
 
   //? 0 Position is claw fully opened, fully on the left, and fully down
 
+  scanColors();
+
+  pushThenSort();
+}
+
+void scanColors() {
+
   //* SCAN THE COLORS
   int lastColorRead = -1;
-  static const int colorReadInterval = 1000;
+  static const int colorReadInterval = 1000; // ms
 
   horizontal.setMaxSpeed(horizontal.maxSpeed() / 2);
   horizontal.moveTo(horizontalLimit);
@@ -54,9 +65,11 @@ void loop() {
     if (now - lastColorRead > colorReadInterval) {
       lastColorRead = now;
 
-      int color = colorSensor.closestColor(colors, colorNumbers, colorCount);
-      if (color != -1 && colorPositions[color] == -1) {
-        colorPositions[color] = horizontal.currentPosition();
+      int color =
+          colorSensor.closestColor(colors, colorNumbers, totalColorCount);
+
+      if (color != -1 && blockPositions[color] == -1) {
+        blockPositions[color] = horizontal.currentPosition() + blockScanOffset;
       }
     }
 
@@ -64,4 +77,42 @@ void loop() {
   }
 
   horizontal.setMaxSpeed(horizontal.maxSpeed() * 2);
+}
+
+void pushThenSort() {
+
+  //* SORT THE COLORS BY FIRST PUSHING THEM TO THE RIGHT
+
+  // grab last block
+  int lastBlockPos = -1;
+  for (const int &blockPos : blockPositions)
+    lastBlockPos = max(lastBlockPos, blockPos);
+
+  horizontal.runToNewPosition(lastBlockPos);
+
+  // grab
+  claw.runToNewPosition(clawLimit);
+
+  // push
+  horizontal.runToNewPosition(blockCount * blockWidth); // - 0.5 * blockWidth);
+
+  // release
+  claw.runToNewPosition(0);
+
+  // calculate new positions
+  int maxCheckPos = horizontalLimit;
+  for (int i = blockCount - 1; i >= 0; i--) {
+    // find max position less than maxCheckPos
+    int maxPos = -1;
+    int maxColor = -1;
+    for (int j = 0; j < blockCount; j++) {
+      if (blockPositions[j] > blockPositions[maxColor] &&
+          blockPositions[j] < maxCheckPos) {
+        maxColor = j;
+      }
+    }
+    maxCheckPos = maxPos;
+
+    blockPositions[maxColor] = i * blockWidth + blockWidth / 2;
+  }
 }
